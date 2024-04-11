c83c2caef3546cd65cdb74229d810a1658bac721
ZahidFlores <zahid.flores@uadec.edu.mx>
2024-04-11 11:05:41 -0600

    proceso de script actualizado

--- Script.py
+++ Script.py
@@ -1,21 +1,17 @@
 import mysql.connector
 
-config = {
-    'user': 'root',
-    'password': 'admin1',
-    'host': 'db',
-    'port': '3306',
-    'database': 'db_test'
-}
-## esta es una prueba
-conn = mysql.connector.connect(**config)
-cursor = conn.cursor()
-query = "SELECT * FROM users"
-cursor.execute(query)
-results = cursor.fetchall()
-for row in results:
+mydb = mysql.connector.connect(
+    host="mysql_container",
+    user="root",
+    password="contraseña",
+    database="db_test"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM users")
+
+for row in mycursor.fetchall():
     print(row)
-cursor.close()
-conn.close()
+mydb.close()
 
 
